7c06b8ef0858cddcd27c280d5f1d805a31b84556
Brandon Johns <56181912+brandonjohns914@users.noreply.github.com>
2024-07-09 12:19:56 -0700

    first commit

---
+++ iTour/iTour/iTourApp.swift
@@ -0,0 +1,17 @@
+//
+//  iTourApp.swift
+//  iTour
+//
+//  Created by Brandon Johns on 7/9/24.
+//
+
+import SwiftUI
+
+@main
+struct iTourApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
